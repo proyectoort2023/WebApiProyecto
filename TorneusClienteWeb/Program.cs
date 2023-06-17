795e0680f3f1f87766a47c6291324f4c0fa36573
proyectoort2023 <af245580@fi365.ort.edu.uy>
2023-06-17 19:21:32 -0300

    dev: confección del login

--- TorneusClienteWeb/Program.cs
+++ TorneusClienteWeb/Program.cs
@@ -1,6 +1,7 @@
 using BlazorTorneusClient.Servicios;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using TorneusClienteWeb;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,6 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped<ServicioMenu>();
 
