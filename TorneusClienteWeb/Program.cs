9bbb6297127abdb65a89f6ba1aa0c35e8bf63f13
proyectoort2023 <af245580@fi365.ort.edu.uy>
2023-06-16 23:49:56 -0300

    dev: Estructura inicial Front End realizado

--- TorneusClienteWeb/Program.cs
+++ TorneusClienteWeb/Program.cs
@@ -1,3 +1,4 @@
+using BlazorTorneusClient.Servicios;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TorneusClienteWeb;
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ServicioMenu>();
+
+
+
 
 await builder.Build().RunAsync();
---
+++ TorneusClienteWeb/Servicios/ServicioMenu.cs
@@ -0,0 +1,28 @@
+﻿
+using BlazorTorneusClient.Modelos;
+
+namespace BlazorTorneusClient.Servicios
+{
+    public class ServicioMenu
+    {
+        private List<MenuItem> items = new List<MenuItem>();
+
+        public event Action OnMenuUpdated;
+
+        public void ActualizarItems(List<MenuItem> nuevoMenuItems)
+        {
+            items = nuevoMenuItems;
+            ActualizarListado();
+        }
+
+        private void ActualizarListado()
+        {
+            OnMenuUpdated?.Invoke();
+        }
+
+        public List<MenuItem> ObtenerItemsMenu()
+        {
+            return items;
+        }
+    }
+}
