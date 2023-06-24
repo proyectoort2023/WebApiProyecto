1e06b67de746cd9ee92afe9f774acfae6b8b1f9d
proyectoort2023 <af245580@fi365.ort.edu.uy>
2023-06-24 00:13:22 -0300

    dev: uso de form en inicio de sesion

--- DTOs Compartidos/DTOs/LoginDTO.cs
+++ DTOs Compartidos/DTOs/LoginDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Negocio.DTOs
 {
     public class LoginDTO
     {
-
+        [Required(ErrorMessage = "Obligatorio")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$", ErrorMessage = "El correo no es válido.")]
         public string Mail { get; set; }
+        [Required(ErrorMessage = "Obligatorio")]
+        [StringLength(30, ErrorMessage = "Minimo 5 caracteres", MinimumLength = 5)]
         public string Pass { get; set; }
     }
 }
