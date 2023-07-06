ba9b1b88fa67206804909a468f59e4fb1efb37bf
proyectoort2023 <af245580@fi365.ort.edu.uy>
2023-07-05 22:15:29 -0300

    dev: parte de inscripcion de equipos, backend
    RF-17

---
+++ Negocio/InscripcionService.cs
@@ -0,0 +1,64 @@
+﻿using BDTorneus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Negocio.Validaciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class InscripcionService
+    {
+
+        private readonly TorneoContext _db;
+
+        public InscripcionService(TorneoContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<List<Inscripcion>> ObtenerInscripcionesSegunUsuario(int usuarioId)
+        {
+            try
+            {
+                if (usuarioId < 0) throw new Exception("El usuario no se encuentra para obtener las inscripciones");
+
+                List<Inscripcion> inscripciones = await _db.Inscripciones.Where(w => w.UsuarioId == usuarioId).ToListAsync();
+
+                if (inscripciones == null) throw new Exception("No hay inscricpiones válidas");
+
+                return inscripciones;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Inscripcion> AgregarNuevaInscripcion(Inscripcion inscripcion)
+        {
+            try
+            {
+                var inscrpcionNueva = await _db.Inscripciones.AddAsync(inscripcion);
+                await _db.SaveChangesAsync();
+
+                return inscrpcionNueva.Entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ TorneusClienteWeb/Servicios de Datos/InscripcionServicioDatos.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Components;
+using Negocio.DTOs;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using TorneusClienteWeb.Servicios;
+
+namespace TorneusClienteWeb.Servicios_de_Datos
+{
+    public class InscripcionServicioDatos
+    {
+        private readonly HttpClient _httpClient;
+        [Inject] private UsuarioServicio _usuarioServicio { get; set; }
+
+
+        public InscripcionServicioDatos(HttpClient httpClient, UsuarioServicio usuarioServicio)
+        {
+            _httpClient = httpClient;
+            _usuarioServicio = usuarioServicio;
+        }
+
+
+        public async Task<List<InscripcionDTO>> ObtenerInscripcionesDeUsuario(int idUsuario)
+        {
+
+            try
+            {
+                if (idUsuario < 1) throw new Exception("No usuario no existe");
+
+                string token = _usuarioServicio.ObtenerUsuarioLogueado().Token;
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var response = await _httpClient.GetAsync($"api/Inscripcion/Listado/{idUsuario}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var inscripciones = JsonConvert.DeserializeObject<List<InscripcionDTO>>(content);
+                    return inscripciones;
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    throw new Exception(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+
+
+        public async Task<InscripcionDTO> ObtenerTorneosVigentes(InscripcionDTO inscripcionDTO)
+        {
+
+            try
+            {
+                string token = _usuarioServicio.ObtenerUsuarioLogueado().Token;
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var response = await _httpClient.PostAsJsonAsync("api/Inscripcion/Agregar", inscripcionDTO);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    var inscripcion = JsonConvert.DeserializeObject<InscripcionDTO>(content);
+                    return inscripcion;
+                }
+                else
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    throw new Exception(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
